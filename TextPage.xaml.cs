4a87ddcdac3899787a5adfd66890985ff64cf4ba
xideme <soopka@gmail.com>
2024-08-27 09:35:19 +0300

    Home Page

--- TextPage.xaml.cs
+++ TextPage.xaml.cs
@@ -1,9 +1,45 @@
+
 namespace MobileApp1;
 
 public partial class TextPage : ContentPage
 {
-	public TextPage()
+	Label lbl;
+	Editor ed;
+	Button btn_back, btn_forward, btn_home;
+	HorizontalStackLayout hsl;
+	List<string> buttons=new List<string> { "Tagasi", "Avaleht", "Edasi"};
+
+    public TextPage()
 	{
-		InitializeComponent();
+		lbl = new Label
+		{
+			Text = "Pealkiri",
+			TextColor = Color.FromRgb(200, 0, 0),
+			FontFamily = "BungeeTint Regular"
+		};
+		ed = new Editor
+		{
+			Placeholder = "Sisesta Siis tekst",
+			PlaceholderColor = Color.FromRgb(50,100,100),
+			TextColor = new Color(255,255,255),
+			BackgroundColor= Color.FromRgb(10,50,10),
+
+
+		};
+		hsl = new HorizontalStackLayout { };
+		for (int i = 0; i < 3; i++)
+		{
+			Button b = new Button
+			{
+				Text = buttons[i],
+			};
+			hsl.Add(b);
+		}
+
+		VerticalStackLayout vst = new VerticalStackLayout
+		{
+			Children = { lbl, ed, hsl },
+
+		};
 	}
 }
