e30139e18c4eea0f77e9d27aea264dc1886526eb
xideme <soopka@gmail.com>
2024-09-16 09:49:37 +0300

    PickerImage

--- DateTimeProject/PickerImagePage.xaml.cs
+++ DateTimeProject/PickerImagePage.xaml.cs
@@ -1,68 +1,82 @@
 
 namespace DateTimeProject;
 
+
 public partial class PickerImagePage : ContentPage
 {
-	Grid gr4x1, gr3x3;
-	Picker picker;
-	Image img;
-	Switch s_pilt, s_grid;
-	public PickerImagePage(int k)
-	{
-		gr4x1 = new Grid
-		{
+    Grid gr4x1, gr3x3;
+    Picker picker;
+    Image img;
+    Switch s_img, s_grid;
+    public PickerImagePage(int k)
+    {
+        gr4x1 = new Grid
+        {
             RowDefinitions =
-			{
-					new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
-					new RowDefinition { Height = new GridLength(3, GridUnitType.Star) },
-					new RowDefinition { Height = new GridLength(3, GridUnitType.Star) },
-					new RowDefinition { Height = new GridLength(1, GridUnitType.Star) }
-			},
-			ColumnDefinitions =
-			{
-				new ColumnDefinition{Width=new GridLength(1, GridUnitType.Star) },
-                new ColumnDefinition{Width=new GridLength(1, GridUnitType.Star) }
-            }
-		};
-		picker = new Picker();
-		{
-			Title = "Pildid";
-		};
-		picker.Items.Add("1. Pilt");
-		picker.Items.Add("2. Pilt");
-		picker.Items.Add("3. Pilt");
-		picker.SelectedIndexChanged += Piltide_Valik;
-		s_pilt.Toggled += Kuva_Peida_pilt;
-		img = new Image
-		{
-			Source = "dotnet_bot.png"
-		};
-		s_pilt = new Switch
-		{
-			IsToggled = true,
-			IsEnabled = true
-		};
-		s_grid = new Switch
-		{
-			IsToggled= true,
-			IsEnabled = true
-		};
+                {
+                    new RowDefinition { Height=new GridLength(1, GridUnitType.Star) },
+                    new RowDefinition { Height=new GridLength(3, GridUnitType.Star) },
+                    new RowDefinition { Height=new GridLength(3, GridUnitType.Star) },
+                    new RowDefinition { Height=new GridLength(1, GridUnitType.Star) },
+                },
+            ColumnDefinitions =
+                {
+                    new ColumnDefinition {Width=new GridLength (1, GridUnitType.Star) },
+                    new ColumnDefinition {Width=new GridLength (1, GridUnitType.Star) },
+                },
+        };
+        picker = new Picker
+        {
+            Title = "Images"
+        };
 
-		gr4x1.Add(picker, 0, 0);
-		gr4x1.Add(img, 0, 1);
-		gr4x1.Add(s_pilt, 0, 3);
-		gr4x1.Add(s_grid, 1, 3);
+        picker.Items.Add("First image");
+        picker.Items.Add("Second image");
+        picker.Items.Add("Third image");
+        picker.SelectedIndexChanged += ImageSelection;
 
-		Content = gr4x1;
-	}
+        img = new Image
+        {
+            Source = "dornet_bot.png"
+        };
 
-    private void Kuva_Peida_pilt(object? sender, ToggledEventArgs e)
-    {
-        throw new NotImplementedException();
+        s_img = new Switch
+        {
+            IsToggled = true,
+            IsEnabled = true,
+            HorizontalOptions = LayoutOptions.Center,
+        };
+        s_img.Toggled += S_img_Toggled;
+
+        s_grid = new Switch
+        {
+            IsToggled = false,
+            IsEnabled = false,
+            HorizontalOptions = LayoutOptions.Center,
+        };
+
+        gr4x1.Add(picker, 0, 0);
+        gr4x1.Add(img, 0, 1);
+        gr4x1.Add(s_img, 0, 3);
+        gr4x1.Add(s_grid, 1, 3);
+
+        Content = gr4x1;
     }
 
-    private void Piltide_Valik(object? sender, EventArgs e)
+    private void S_img_Toggled(object? sender, ToggledEventArgs e)
     {
-        //throw new NotImplementedException();
+        if (e.Value)
+        {
+            img.IsVisible = true;
+        }
+        else
+        {
+            img.IsVisible = false;
+        }
     }
-}
+
+    private void ImageSelection(object? sender, EventArgs e)
+        {
+
+        }
+    }
